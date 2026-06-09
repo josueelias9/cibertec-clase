60254bdb6a1d3ad7ee851460ef665c27985cf550
Josue <josueeliashuaman@gmail.com>
2026-06-08 21:23:54 -0500

    fix: actualizar env

--- proyecto/__init__.py
+++ proyecto/__init__.py
@@ -4,6 +4,9 @@ from flask import Flask
 def create_app(test_config=None):
     app = Flask(__name__,instance_relative_config=True)
 
+    app.config.from_mapping(
+        DATABASE=os.path.join(app.instance_path, "flaskr.sqlite"),
+    )
 
     @app.route("/")
     def index():
