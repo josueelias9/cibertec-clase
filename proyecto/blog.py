a46b195f3e3539d280d6fa60b96ff0bd9682f32d
Josue <josueeliashuaman@gmail.com>
2026-06-10 20:40:15 -0500

    feat: agregar endpoint para crear blog

--- proyecto/blog.py
+++ proyecto/blog.py
@@ -1,7 +1,7 @@
 
 
-from flask import Blueprint, render_template
-
+from flask import Blueprint, render_template, request
+from .db import get_db
 
 bp = Blueprint("blog", __name__, url_prefix="/cibertec")
 
@@ -9,6 +9,21 @@ bp = Blueprint("blog", __name__, url_prefix="/cibertec")
 def mostrar_blog():
     return render_template("blog/blog.html")
 
+
+@bp.route("/create", methods=["GET", "POST"])
+def create_blog():
+    if request.method == "POST":
+        titulo = request.form["titulo"]
+        contenido = request.form["content"]
+        author_id = 1
+        db = get_db()
+        db.execute(
+            "INSERT INTO post (title, body, author_id) VALUES (?, ?,?)",
+            (titulo, contenido,author_id)
+        )
+        # Aquí se procesaría el formulario para crear una nueva entrada de blog
+        return {"info":"Nueva entrada de blog creada"}
+    return render_template("blog/create.html")
 
 @bp.route("/eliminar-blog")
 def eliminar_blog():
